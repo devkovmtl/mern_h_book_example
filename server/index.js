4b59f1a4ec8648dd91a5cc00ada8e41a43cf84a2
Mikael <dev.kov.mtl@gmail.com>
2022-05-26 14:46:09 -0400

    server Handling Error

--- server/index.js
+++ server/index.js
@@ -40,6 +40,18 @@ app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
 app.use('/api/users', usersRoute);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something went wrong!';
+
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
 app.listen(PORT || 8080, () => {
   connect();
   console.log(`Server running on port 8800`);
