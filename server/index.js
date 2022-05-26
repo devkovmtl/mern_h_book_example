6b46242d3d8dffb9f5bfb4bd8520086076fcc8ea
Mikael <dev.kov.mtl@gmail.com>
2022-05-26 13:11:07 -0400

    Setup basic routes

--- server/index.js
+++ server/index.js
@@ -1,26 +1,46 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
-dotenv.config();
+import authRoute from './routes/auth.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+import usersRoute from './routes/users.js';
 
 const { PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_DATABASE } =
   process.env;
 
 // CONNECTION TO DB
-const MONGO_OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const connect = async () => {
+  try {
+    const MONGO_OPTIONS = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+    const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+    await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
+  } catch (error) {
+    throw error;
+  }
 };
-const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
-mongoose.connect(MONGO_URI, MONGO_OPTIONS);
-// Get the default connection
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoDB disconnected');
+});
+mongoose.connection.on('connected', () => {
+  console.log('mongoDB connected');
+});
 
 // EXPRESS APP
 const app = express();
 
+// Middleware
+app.use('/api/auth', authRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+app.use('/api/users', usersRoute);
+
 app.listen(PORT || 8080, () => {
+  connect();
   console.log(`Server running on port 8800`);
 });
